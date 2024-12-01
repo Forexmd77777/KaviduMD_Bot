9a6ceba4de5a2bbd36ac658ced8db45791b532d7
TheKingZeuz-MD <thekingofzeuzbaba@gmail.com>
2024-12-02 03:11:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,20 +6,5 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "enter your session",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://pin.it/7qN9HvZaH",
-ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M TheKingZeuz MD WHATSAPP BOT 😊♻️",
-ANTI_LINK: process.env.ANTI_LINK || "true",
-ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || ".",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-AUTO_REACT: process.env.AUTO_REACT || "true",
-HEART_REACT: process.env.HEART_REACT || "true",
-OWNER_REACT: process.env.OWNER_REACT || "true",
-BOT_NAME: process.env.BOT_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
-};
+MONGODB : process.env.MONGODB || "enter mongodb here",
+}"
