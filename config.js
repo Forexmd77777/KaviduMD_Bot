d57b033033bbbbd8cf019f64c9fdd38d01be7f4a
Forexmd77777 <Sevindaihara@gmail.com>
2025-08-22 13:56:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-MONGODB : process.env.MONGODB || "enter mongodb here",
+SESSION_ID: process.env.SESSION_ID || "cFpQBQYS#I4yUuVvwu4exSCY0Dccz9AuqoNuYDj481-dWUeVk_I4",
+MONGODB : process.env.MONGODB || "mongodb+srv://netece6809:SYPmSK4UFWvEnANC@forexdb.0dbqstb.mongodb.net/",
 }"
